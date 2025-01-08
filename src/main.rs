0afb1b20017b9b87c7f3d8d52f7f35f16bc8376d
Illia Levadskyi <3942439@gmail.com>
2025-01-08 22:16:33 +0200

    Creted highlight for text + exit command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+#[allow(unused_imports)]
+use crossterm::{execute, style::{Color, Print, ResetColor, SetForegroundColor}, ExecutableCommand};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, Write};
+use std::process::exit;
+use hostname::get;
+
 fn main() {
-    println!("Hello, world!");
+
+    let username = get_username();
+    let hostname = get_hostname();
+
+    let mut stdout = stdout();
+    
+    loop{
+        let user_and_host = format!("{}{}\n", username, hostname);
+        stdout.execute(SetForegroundColor(Color::DarkGreen))
+            .unwrap()
+            .execute(Print(user_and_host))
+            .unwrap();
+        
+        stdout.execute(ResetColor).unwrap();
+
+        stdout.execute(Print("$ ")).unwrap();
+
+        let mut input = String::new();
+        
+        stdin().read_line(&mut input).expect("Failed to read line");
+
+        let input = input.trim();
+
+        if input == "exit"{
+            exit(0);
+        }
+    }
+}
+
+fn get_username() -> String{
+    std::env::var("USER").or_else(|_| std::env::var("USERNAME")).unwrap_or_else(|_| "unknown".to_string())
+}
+
+fn get_hostname() -> String{
+    get()
+        .map(|h| h.to_string_lossy().into_owned())
+        .unwrap_or_else(|_| "unknown".to_string())
 }
